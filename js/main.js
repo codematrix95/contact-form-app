510a97e37935025b76d94c4e3f7c4a61b69ed1c3
codematrix95 <codematrix95@gmail.com>
2024-09-04 21:48:16 -0500

    Updated how errors are handled so you can see them sequential

--- js/main.js
+++ js/main.js
@@ -3,192 +3,237 @@ const body = document.querySelector("body");
 const form = document.querySelector("form");
 const labels = document.querySelectorAll("label");
 const inputCntrs = document.querySelectorAll(".inputCntr");
-const errors = document.querySelectorAll("p");
-const submit = document.querySelector("#submit");
+const errMsg = document.querySelectorAll(".errMsg");
 
 const fname = document.querySelector("#fname");
 const lname = document.querySelector("#lname");
 const email = document.querySelector("#email");
-
 const genquiry = document.querySelector("#genquiry");
+const srequest = document.querySelector("#srequest");
+const msg = document.querySelector("#msg");
+const consent = document.querySelector("#consent");
+const submit = document.querySelector("#submit");
 
-const emailValidation = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
+const stuff = (inputsArr) => {
+    console.log(inputsArr);
+};
 
-console.log(inputCntrs);
-console.log(errors);
+const isNameValid = (input, i) => {
+    input.value = input.value.replace(/\s+/g, "");
+    const nameValidation = /[^a-zA-Z]+/g;
+    let name = nameValidation.test(input.value);
+    if (name === false && input.value !== "") {
+        errMsg[i].style.color = "red";
+        input.setAttribute("aria-invalid", "false");
+    } else {
+        errMsg[i].style.color = "blue";
+        input.setAttribute("aria-invalid", "true");
+    }
+};
+
+const isEmailValid = (input, i) => {
+    input.value = input.value.replace(/\s+/g, "");
+    const emailValidation = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
+    let email = emailValidation.test(input.value);
+    if (email === false) {
+        errMsg[i].style.color = "blue";
+        input.setAttribute("aria-invalid", "true");
+    } else {
+        errMsg[i].style.color = "red";
+        input.setAttribute("aria-invalid", "false");
+    }
+};
+
+const inputSuccess = (input, i) => {
+    input.setAttribute("aria-invalid", "false");
+    errMsg[i].style.color = "red";
+};
+
+const inputError = (input, i) => {
+    input.setAttribute("aria-invalid", "true");
+    errMsg[i].style.color = "blue";
+};
+
+const isQtypeValid = (i) => {
+    let fieldArr = [...inputCntrs[i].children];
+    let buttons = fieldArr.filter((e) => {
+        return e.type === "radio";
+    });
+    let buttonChecked = fieldArr.filter((e) => {
+        return e.checked === true;
+    });
+
+    if (buttonChecked.length === 0) {
+        buttons.forEach((e) => {
+            e.setAttribute("aria-invalid", "true");
+        });
+        errMsg[i].style.color = "blue";
+    } else {
+        buttons.forEach((e) => {
+            e.setAttribute("aria-invalid", "false");
+        });
+        errMsg[i].style.color = "red";
+    }
+};
+
+const checkInputOnblur = (input, i) => {
+    if (input.id === "fname" || input.id === "lname") {
+        isNameValid(input, i);
+    }
+    if (input.id === "email") {
+        isEmailValid(input, i);
+    }
+    if (input.parentNode.type === "fieldset") {
+        isQtypeValid(i);
+    }
+    if (input.id === "msg") {
+        if (input.value === "") {
+            inputError(input, i);
+        } else {
+            inputSuccess(input, i);
+        }
+    }
+    if (input.id === "consent") {
+        if (input.checked === false) {
+            inputError(input, i);
+        }
+    }
+};
+
+const checkInputOnclick = (input, i) => {
+    if (input.parentNode.type === "fieldset") {
+        filterFieldset(i);
+    }
+    if (input.id === "consent") {
+        inputSuccess(input, i);
+    }
+};
+
+for (let i = 0; i < inputCntrs.length; i++) {
+    for (let x = 0; x < inputCntrs[i].children.length; x++) {
+        let input = inputCntrs[i].children[x];
+        if (input.tagName === "INPUT") {
+            input.onblur = () => {
+                checkInputOnblur(input, i);
+            };
+            input.onclick = () => {
+                checkInputOnclick(input, i);
+            };
+        }
+    }
+}
 
 submit.onclick = (e) => {
-    // let errorArr = [];
-    // let successArr = [];
-
-    // let textArr = [];
-    // let qtypeArr = [];
-    // let checkboxArr = [];
-
-    const errorFunc = (i, x) => {
-        errors[i].style.color = "blue";
-        inputCntrs[i].children[x].setAttribute("aria-invalid", "true");
-    };
-
-    const successFunc = (i, x) => {
-        errors[i].style.color = "red";
-        inputCntrs[i].children[x].setAttribute("aria-invalid", "false");
-    };
-
     let inputsArr = [];
-    // let qtypeArr = [];
-    let qtypeChecked = [];
-    let qtypeUnchecked = [];
-
-    const qtypeCheck = (i, x, qtypeChecked) => {
-        let qtypeCntr = inputCntrs[i].children[x].parentElement.children;
-        for (let i = 0; i < qtypeCntr.length; i++) {
-            if (
-                qtypeCntr[i].type === "radio" &&
-                qtypeCntr[i].checked === true
-            ) {
-                qtypeChecked.push(qtypeCntr[i]);
-                console.log(qtypeChecked);
-            } else {
-                if (
-                    qtypeCntr[i].type === "radio" &&
-                    qtypeCntr[i].checked === false
-                ) {
-                    if (qtypeChecked.length < 1) {
-                        qtypeChecked.push(qtypeCntr[i]);
-                        console.log(qtypeChecked);
-                    }
-                }
-            }
-        }
-    };
 
     e.preventDefault();
     for (let i = 0; i < inputCntrs.length; i++) {
-        // console.log(inputCntrs.length)
-        for (let x = 0; x < inputCntrs[i].children.length; x++) {
-            if (inputCntrs[i].children[x].tagName === "INPUT") {
-                if (inputCntrs[i].children[x].value === "") {
-                    errorFunc(i, x, inputsArr);
-                    inputsArr.push(inputCntrs[i].children[x]);
-                } else {
-                    if (inputCntrs[i].children[x].id === "email") {
-                        let isValidEmail = emailValidation.test(
-                            inputCntrs[i].children[x].value
-                        );
-                        if (isValidEmail === false) {
-                            console.log("invalid email");
-                            errorFunc(i, x, inputsArr);
-                            inputsArr.push(inputCntrs[i].children[x]);
-                        } else {
-                            successFunc(i, x);
-                        }
-                    } else {
-                        if (
-                            inputCntrs[i].children[x].value === "on" &&
-                            inputCntrs[i].children[x].checked === false
-                        ) {
-                            if (inputCntrs[i].children[x].type === "radio") {
-                                // qtypeArr.push(inputCntrs[i].children[x]);
-                                // console.log(inputCntrs[i].children[x].parentElement.children.length)
-                                if (qtypeChecked.length === 0) {
-                                    qtypeCheck(i, x, qtypeChecked);
-                                    inputsArr.push(qtypeChecked[0]);
-                                }
-                            } else {
-                                inputsArr.push(inputCntrs[i].children[x]);
-                            }
+        if (inputCntrs[i].className === "inputCntr") {
+            inputVerification(i, inputsArr);
+        } else if (inputCntrs[i].className === "inputCntr fieldset") {
+            qtypeVerification(i, inputsArr);
+        }
+    }
 
-                            errorFunc(i, x);
-                        } else {
-                            successFunc(i, x);
-                        }
+    if (document.activeElement) {
+        console.log(document.activeElement);
+        document.activeElement.blur();
+    }
+
+    if (inputsArr.length !== 0) {
+        // inputsArr[0].focus();
+        // array = [];
+        // array = inputsArr.slice(0);
+        // console.log("#" + inputsArr[0].id)
+        stuff(inputsArr);
+    }
+
+    // console.log(array);
+    // console.log(inputsArr);
+};
+
+const inputVerification = (i, inputsArr) => {
+    for (let x = 0; x < inputCntrs[i].children.length; x++) {
+        let input = inputCntrs[i].children[x];
+        if (input.tagName === "INPUT") {
+            if (input.value === "") {
+                inputError(i, x, inputsArr);
+            } else {
+                if (input.id === "email") {
+                    emailVerification(i, x, inputsArr);
+                } else {
+                    if (input.value === "on" && input.checked === false) {
+                        inputError(i, x, inputsArr);
+                    } else {
+                        inputSuccess(i, x);
                     }
                 }
             }
         }
     }
-
-    // Boooooooobbbbbsssss
-
-    // console.log(qtypeArr);
-    console.log(inputsArr);
-
-    // for (let i = 0; i < textArr.length; i++) {
-    //     if (textArr[i].type === "text" && textArr[i].value === "") {
-    //             errorArr.push(textArr[i]);
-    //     } else {
-    //         if (textArr[i].type !== "email") {
-    //             successArr.push(textArr[i]);
-    //         }
-    //     }
-
-    //     if (textArr[i].type === "email") {
-    //         let isValidEmail = emailValidation.test(textArr[i].value);
-    //         if (isValidEmail === false) {
-    //             console.log("invalid email");
-    //             errorArr.push(textArr[i]);
-    //         } else {
-    //             console.log("email valid");
-    //             successArr.push(textArr[i]);
-    //         }
-    //     }
-    // }
-
-    // console.log(successArr);
-    // console.log(errorArr);
-
-    // console.log(textArr);
-    // console.log(qtypeArr);
-    // console.log(checkboxArr);
-
-    if (document.activeElement) {
-        document.activeElement.blur();
-    }
-
-    // errorArr[0].focus();
-
-    // console.log(inputsArr);
-    // console.log(radioArr);
 };
 
-// for (let x = 0; x < inputCntrs[i].children.length; x++) {
-//     if (inputCntrs[i].children[x].tagName === "INPUT") {
-//         let input = inputCntrs[i].children[x];
-//         if (input.type === "text" || input.type === "email") {
-//             textArr.push(input);
-//         } else {
-//             if (input.type === "radio") {
-//                 qtypeArr.push(input);
-//             } else {
-//                 if (input.type === "checkbox") {
-//                     checkboxArr.push(input);
-//                 }
-//             }
-//         }
-//     }
-// }
+const qtypeVerification = (i, inputsArr) => {
+    let fieldsetTrueArr = [];
+    let fieldsetFalseArr = [];
+    let checked = false;
 
-// for (let i = 0; i < qtypeArr.length; i++) {
-//     if (qtypeArr[i].checked === true) {
-//         successArr.push(qtypeArr[i]);
-//         qtypeArr = [];
-//         console.log("checked");
-//     }
-// }
+    for (let x = 0; x < inputCntrs[i].children.length; x++) {
+        let input = inputCntrs[i].children[x];
+        if (input.type === "radio") {
+            fieldsetTrueArr.push(input);
+            if (input.checked === false) {
+                if (checked === false) {
+                    fieldsetFalseArr.push(input);
+                }
+            } else {
+                if (input.checked === true) {
+                    checked = true;
+                    fieldsetFalseArr = [];
+                }
+            }
+        }
+    }
 
-// if (qtypeArr.length > 0) {
-//     errorArr.push(qtypeArr[0]);
-// }
+    if (fieldsetFalseArr.length !== 0) {
+        inputsArr.push(fieldsetFalseArr[0]);
+        fieldsetFalseArr.forEach((e) => {
+            e.setAttribute("aria-invalid", "true");
+        });
+        errMsg[i].style.color = "blue";
+        // errorArr.push(errMsg[i]);
+    } else {
+        fieldsetTrueArr.forEach((e) => {
+            e.setAttribute("aria-invalid", "false");
+        });
+        errMsg[i].style.color = "red";
+    }
+};
 
-// if (checkboxArr[0].checked === true) {
-//     successArr.push(checkboxArr[0]);
-// } else {
-//     errorArr.push(checkboxArr[0]);
-// }
+const emailVerification = (i, x, inputsArr) => {
+    const emailValidation = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
 
-// for (let i = 0; i < errorArr.length; i++) {
-//     let e = errorArr[i];
-//     errors[i].style.color = "blue";
-//     e.setAttribute("aria-invalid", "true");
-// }
+    let input = inputCntrs[i].children[x];
+    let isValidEmail = emailValidation.test(input.value);
+    if (isValidEmail === false) {
+        console.log(input);
+        inputError(i, x, inputsArr);
+    } else {
+        inputSuccess(i, x);
+    }
+};
+
+// const inputError = (i, x, inputsArr) => {
+//     let input = inputCntrs[i].children[x];
+//     errMsg[i].style.color = "blue";
+//     input.setAttribute("aria-invalid", "true");
+//     inputsArr.push(input);
+//     // errorArr.push(errMsg[i]);
+//     // console.log(errorArr);
+// };
+
+// const inputSuccess = (i, x) => {
+//     let input = inputCntrs[i].children[x];
+//     errMsg[i].style.color = "red";
+//     input.setAttribute("aria-invalid", "false");
+// };
